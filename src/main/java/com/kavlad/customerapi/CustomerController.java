c06563dca0bc30664d693c8ba1c03eec7e7bd844
Mohammad Atallah <matallah@Benos-MacBook-Pro.local>
2020-04-19 22:24:39 +0400

    Add some comments

--- src/main/java/com/kavlad/customerapi/CustomerController.java
+++ src/main/java/com/kavlad/customerapi/CustomerController.java
@@ -9,6 +9,9 @@ import java.util.HashMap;
 public class CustomerController {
 
     private HashMap<Integer, Customer> Customers = new HashMap<Integer, Customer>() {{
+
+        // Add a couple of customers for testing...
+
         put(1, new Customer(
                 "firstName1",
                 "lastName1",
@@ -35,34 +38,40 @@ public class CustomerController {
         ));
     }};
 
+    // Get all customers
     @RequestMapping(value = "/customer", method = RequestMethod.GET)
     public HashMap<Integer, Customer> getCustomers() {
         return this.Customers;
     }
 
+    // Get customer with the id defined
     @RequestMapping(value = "/customer/{id}", method = RequestMethod.GET)
     public Customer getCustomer(@PathVariable("id") int id) {
         return this.Customers.get(id);
     }
 
+    // Create a customer
     @RequestMapping(value = "/customer", method = RequestMethod.POST)
     public HttpStatus createCustomers(@RequestBody Customer payload) {
         this.Customers.put(this.Customers.size() + 1, payload);
         return HttpStatus.OK;
     }
 
+    // Create an address for this customer
     @RequestMapping(value = "/customer/{id}/address", method = RequestMethod.POST)
     public HttpStatus createAddress(@PathVariable("id") int id, @RequestBody Address payload) {
         this.Customers.get(id).setAddress(payload);
         return HttpStatus.OK;
     }
 
+    // Delete the address of the customer
     @RequestMapping(value = "/customer/{id}/address", method = RequestMethod.DELETE)
     public HttpStatus deleteAddress(@PathVariable("id") int id) {
         this.Customers.get(id).setAddress(null);
         return HttpStatus.OK;
     }
 
+    // Get all customers from this city
     @RequestMapping(value = "/city/{name}", method = RequestMethod.GET)
     public HashMap<Integer, Customer> getCustomersByCity(@PathVariable("name") String city) {
         HashMap<Integer, Customer> response = new HashMap<Integer, Customer>();
@@ -74,6 +83,7 @@ public class CustomerController {
         return response;
     }
 
+    // Get all customers starting with the following prefix phone number
     @RequestMapping(value = "/phone/{prefix}", method = RequestMethod.GET)
     public HashMap<Integer, Customer> getCustomersByPhone(@PathVariable("prefix") String prefix) {
         HashMap<Integer, Customer> response = new HashMap<Integer, Customer>();
