2129dc76b561c4e8ae562a486c13010c20855dcd
Mohammad Atallah <matallah@Benos-MacBook-Pro.local>
2020-04-20 11:14:42 +0400

    Handle null Address

--- src/main/java/com/kavlad/customerapi/CustomerController.java
+++ src/main/java/com/kavlad/customerapi/CustomerController.java
@@ -9,7 +9,7 @@ import java.util.Random;
 @RestController
 public class CustomerController {
 
-    private HashMap<Integer, Customer> Customers = new HashMap<Integer, Customer>() {{
+    private HashMap<Integer, Customer> customers = new HashMap<Integer, Customer>() {{
 
         // Add a couple of customers for testing...
 
@@ -42,33 +42,33 @@ public class CustomerController {
     // Get all customers
     @RequestMapping(value = "/customer", method = RequestMethod.GET)
     public HashMap<Integer, Customer> getCustomers() {
-        return this.Customers;
+        return this.customers;
     }
 
     // Get customer with the id defined
     @RequestMapping(value = "/customer/{id}", method = RequestMethod.GET)
     public Customer getCustomer(@PathVariable("id") int id) {
-        return this.Customers.get(id);
+        return this.customers.get(id);
     }
 
     // Create a customer
     @RequestMapping(value = "/customer", method = RequestMethod.POST)
     public HttpStatus createCustomers(@RequestBody Customer payload) {
-        this.Customers.put((new Random().nextInt() & Integer.MAX_VALUE) + 1, payload);
+        this.customers.put((new Random().nextInt() & Integer.MAX_VALUE) + 1, payload);
         return HttpStatus.OK;
     }
 
     // Create an address for this customer
     @RequestMapping(value = "/customer/{id}/address", method = RequestMethod.POST)
     public HttpStatus createAddress(@PathVariable("id") int id, @RequestBody Address payload) {
-        this.Customers.get(id).setAddress(payload);
+        this.customers.get(id).setAddress(payload);
         return HttpStatus.OK;
     }
 
     // Delete the address of the customer
     @RequestMapping(value = "/customer/{id}/address", method = RequestMethod.DELETE)
     public HttpStatus deleteAddress(@PathVariable("id") int id) {
-        this.Customers.get(id).setAddress(null);
+        this.customers.get(id).setAddress(null);
         return HttpStatus.OK;
     }
 
@@ -76,8 +76,10 @@ public class CustomerController {
     @RequestMapping(value = "/city/{name}", method = RequestMethod.GET)
     public HashMap<Integer, Customer> getCustomersByCity(@PathVariable("name") String city) {
         HashMap<Integer, Customer> response = new HashMap<Integer, Customer>();
-        this.Customers.forEach((key, val) -> {
-            if (val.getAddress().getCity().equals(city)) {
+        this.customers.forEach((key, val) -> {
+            if ((val.getAddress() != null) &&
+                    !(val.getAddress().getCity().isEmpty()) &&
+                    (val.getAddress().getCity().equals(city))) {
                 response.put(key, val);
             }
         });
@@ -88,7 +90,7 @@ public class CustomerController {
     @RequestMapping(value = "/phone/{prefix}", method = RequestMethod.GET)
     public HashMap<Integer, Customer> getCustomersByPhone(@PathVariable("prefix") String prefix) {
         HashMap<Integer, Customer> response = new HashMap<Integer, Customer>();
-        this.Customers.forEach((key, val) -> {
+        this.customers.forEach((key, val) -> {
             if (val.getPhoneNumber().startsWith(prefix)) {
                 response.put(key, val);
             }
